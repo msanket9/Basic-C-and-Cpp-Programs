d4c0977f0c92a60343f1f6767d89a7bf2e65e2a6
Sanket Marathe <sanketmarathe80@gmail.com>
2021-10-03 13:37:40 +0530

    Update Simple_Linear_Search.cpp

--- Cpp Codes/Searching Algorithms/Simple_Linear_Search.cpp
+++ Cpp Codes/Searching Algorithms/Simple_Linear_Search.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int arr[10], i, num, index;
     cout<<"Enter 10 Numbers: ";
     for(i=0; i<10; i++)
@@ -9,13 +8,10 @@ int main()
     cout<<"\nEnter a Number to Search: ";
     cin>>num;
     for(i=0; i<10; i++)
-    {
-        if(arr[i]==num)
-        {
+        if(arr[i]==num){
             index = i;
             break;
         }
-    }
     cout<<"\nFound at Index No."<<index;
     cout<<endl;
     return 0;
